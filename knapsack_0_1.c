5d336dec1ccd375d89bcc5c039881a84ecadb0b2
stekap000 <stekap000@gmail.com>
2024-10-25 22:25:31 +0200

    recursive solution with 2d buffer speedup

--- knapsack_0_1.c
+++ knapsack_0_1.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 typedef unsigned int u32;
-typedef unsigned long long u64;
-typedef double f64;
+typedef float f32;
 
 typedef struct Item Item;
 struct Item {
-	f64 weight;
-	f64 value;
+	f32 weight;
+	f32 value;
 };
 
-Item* generate_random_items(u64 n, u32 seed, f64 max_item_weight, f64 max_item_value) {
+Item* generate_random_items(u32 n, u32 seed, f32 max_item_weight, f32 max_item_value) {
 	srand(seed);
 	Item* items = malloc(n * sizeof(Item));
 	if(items) {
-		for(u64 i = 0; i < n; ++i) {
-			items[i].weight = (f64)rand()/(f64)RAND_MAX * max_item_weight;
-			items[i].value = (f64)rand()/(f64)RAND_MAX * max_item_value;
+		for(u32 i = 0; i < n; ++i) {
+			items[i].weight = (u32)((f32)rand()/(f32)RAND_MAX * max_item_weight);
+			items[i].value = (u32)((f32)rand()/(f32)RAND_MAX * max_item_value);
 		}
 		return items;
 	}
@@ -26,11 +25,19 @@ Item* generate_random_items(u64 n, u32 seed, f64 max_item_weight, f64 max_item_v
 	}
 }
 
-void print_items(Item* items, u64 n) {
-	for(u64 i = 0; i < n; ++i) {
+void print_items(Item* items, u32 n) {
+	for(u32 i = 0; i < n; ++i) {
 		printf("( %9.6lf, %9.6lf )\n", items[i].weight, items[i].value);
 	}
 }
+
+// =========================================================================================================
+// MOST BASIC RECURSIVE SOLUTION
+//
+// Time complexity  : O(2^n)
+// Space complexity : O(2^n * (stack frame size)) for total used memory
+//                    O(n * (stack frame size))   for memory used at arbitrary time
+// =========================================================================================================
 
 // Returns the maximum value that can be achieved while not exceeding knapsack space.
 // Also returns maximum reached weight.
@@ -43,7 +50,14 @@ void print_items(Item* items, u64 n) {
 // It struggles even with small sample size that are not trivial (like n = 50, knapsack_space = 500).
 // This is because in that case, if items can't reduce space to zero, algorithm will recurse
 // all the way down until n = 0 (which is horrible for even a small n).
-Item recursive_solution(Item* items, u64 n, f64 knapsack_space) {
+
+// This solution does not require additional explicit space for storing computation information,
+// but that is of little relevancy when it eats stack with function stack frames.
+
+// Asymptotic time complexity is O(2^n) since we traverse all possible paths and this can lead to
+// repetitions, where we calculate function value multiple times for the same input.
+
+Item recursive_solution(Item* items, u32 n, f32 knapsack_space) {
 	// If no more space or no more items.
 	if(knapsack_space <= 0 || n == 0) {
 		return (Item){0, 0};
@@ -71,16 +85,76 @@ Item recursive_solution(Item* items, u64 n, f64 knapsack_space) {
 	}
 }
 
+// =========================================================================================================
+// RECURSIVE SOLUTION WITH 2D TABLE (ITEM WEIGHTS AND KNAPSACK WEIGHT MUST BE INTEGERS IN THIS APPROACH)
+// Time complexity  :
+// Space complexity :
+//                   
+// =========================================================================================================
+
+Item solve(Item* items, u32 n, u32 knapsack_space, Item* buffer, u32 total_knapsack_space) {
+	u32 base_buffer_index = ((n-1) * total_knapsack_space + (knapsack_space - 1));
+	
+	// If no more space or no more items.
+	if(knapsack_space <= 0 || n == 0) {
+		return (Item){0, 0};
+	}
+	
+	if(buffer[base_buffer_index].value >= 0) {
+		return buffer[base_buffer_index];
+	}
+	
+	// Exclude currently last item if its weight exceeds knapsack space and recurse further.
+	if((u32)items[n-1].weight > knapsack_space) {
+		buffer[base_buffer_index] = solve(items, n-1, knapsack_space, buffer, total_knapsack_space);
+		return buffer[base_buffer_index];
+	}
+
+	// Go through cases where we exclude currently last item and where we include it.
+	// If we include it, then we count its value and weight and reduce current knapsack space.
+	
+	// We pick the path that gives maximum value for the current step (local computational maximum).
+	Item value_excluded = solve(items, n-1, knapsack_space, buffer, total_knapsack_space);
+	Item value_included = solve(items, n-1, knapsack_space - (u32)items[n-1].weight, buffer, total_knapsack_space);
+
+	buffer[base_buffer_index] = value_excluded;
+	buffer[base_buffer_index - (u32)items[n-1].weight] = value_included;
+	
+	value_included.weight += (u32)items[n-1].weight;
+	value_included.value  += (u32)items[n-1].value;
+
+	if(value_excluded.value > value_included.value) {
+		return value_excluded;
+	}
+	else {
+		return value_included;
+	}
+}
+
+Item recursive_solution_with_2D_buffer_and_integer_weights(Item* items, u32 n, u32 knapsack_space) {
+	u32 buffer_size = n*knapsack_space;
+	Item* buffer = malloc(buffer_size*sizeof(Item));
+
+	for(u32 i = 0; i < buffer_size; ++i) {
+		buffer[i].value = -1;
+	}
+	
+	Item result = solve(items, n, knapsack_space, buffer, knapsack_space);
+	free(buffer);
+	return result;
+}
+
 int main(void) {
 	u32 seed = 1234;
-	u64 n = 10;
-	f64 knapsack_space = 150;
+	u32 n = 10;
+	f32 knapsack_space = 150;
 	(void)knapsack_space;
 	
 	Item* items = generate_random_items(n, seed, 100, 100);
 	if(items) {
 		print_items(items, n);
-		Item result = recursive_solution(items, n, knapsack_space);
+		//Item result = recursive_solution(items, n, knapsack_space);
+		Item result = recursive_solution_with_2D_buffer_and_integer_weights(items, n, (u32)knapsack_space);
 		printf("Maximum weight: %lf\n", result.weight);
 		printf("Maximum value : %lf\n", result.value);
 	}
